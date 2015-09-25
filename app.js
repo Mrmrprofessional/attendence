1facf1e175513344e40706e5a570a6144b401699
Mike <michael.rapp358@gmail.com>
2015-09-25 12:46:47 -0700

    add readme

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var attendence = angular.module('attendence', ['ui.router']);
 
 attendence.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('home', {
-    url: "",
-    templateUrl: "partials/home.html"
-  });
+    $stateProvider.state('home', {
+        url: "",
+        templateUrl: "partials/home.html"
+    });
 
-  $stateProvider.state('signIn', {
-    url: "/signIn",
-    templateUrl: "partials/signIn.html",
-    controller: 'SignInCtrl'
-  });
+    $stateProvider.state('signIn', {
+        url: "/signIn",
+        templateUrl: "partials/signIn.html",
+        controller: 'SignInCtrl'
+    });
 
-  $stateProvider.state('here', {
-    url: "/here",
-    templateUrl: "partials/here.html",
-    controller: 'HereCtrl'
-  });
+    $stateProvider.state('here', {
+        url: "/here",
+        templateUrl: "partials/here.html",
+        controller: 'HereCtrl'
+    });
 });
